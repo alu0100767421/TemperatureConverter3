14758dff6a76c20131a941e6f53c16478dfd1bbb
javierpestanoron <javipestano@gmail.com>
2015-10-25 10:04:20 +0000

    Update Karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -28,15 +28,17 @@ module.exports = function(config) {
 
 
     // list of files to exclude
-    exclude: [
-      'gulpfile.js'
+      exclude: [
+      'gulpfile.js',
+      'static-server.js'
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-         'test.html': ['html2js']
+   preprocessors: {
+      'test/test_blanket.html': ['html2js'],
+      'test/index.html': ['html2js']
     },
 
 
